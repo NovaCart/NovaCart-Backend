b8cb9a97ea31fc64286c619a164950ea12aa8af3
Nimesha Abesinghe <abesinghenimesha20@gmail.com>
2024-09-04 15:26:45 +0530

    added packages, created DataContext

---
+++ NovaCart-Backend/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NovaCart_Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.; initial Catalog=novacartdb ; User Id=sa; password=1234; TrustServerCertificate= True");
+        }
+
+
+    }
+}
